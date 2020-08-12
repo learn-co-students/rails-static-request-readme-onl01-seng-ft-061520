8061590a3a1d4113e0db2148ea7fa44a52c698e7
devWRM <mcwen@comcast.net>
2020-08-12 10:45:34 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # http://localhost:3000/about       <= HTTP request sent here
+
+  get 'about', to: 'static#about'
+  
+
 end
